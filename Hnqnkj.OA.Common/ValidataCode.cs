7f7909ab9d39e6fd0b27f89e1eaaa53570d1b3e7
tyz <812130678@qq.com>
2018-07-26 16:11:35 +0800

    1

---
+++ Hnqnkj.OA.Common/ValidataCode.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hnqnkj.OA.Common { 
+    public class ValidataCode
+    {
+        public byte[] CreateValidateGraphic(string validateCode)
+        {
+            Bitmap image = new Bitmap((int)Math.Ceiling(validateCode.Length * 12.0), 22);
+            Graphics g = Graphics.FromImage(image);
+
+            try
+            {
+                Random random = new Random();
+                g.Clear(Color.White);
+                for (int i = 0; i < 25; i++)
+                {
+                    int x1 = random.Next(image.Width);
+                    int x2 = random.Next(image.Width);
+                    int y1 = random.Next(image.Height);
+                    int y2 = random.Next(image.Height);
+                    g.DrawLine(new Pen(Color.Silver), x1, y1, x2, y2);
+                }
+                Font font = new Font("Arial", 12, (FontStyle.Bold | FontStyle.Italic));
+                LinearGradientBrush brush = new LinearGradientBrush(new Rectangle(0, 0, image.Width, image.Height), Color.Blue, Color.DarkRed, 1.2f, true);
+                g.DrawString(validateCode, font, brush, 3, 2);
+                for (int i = 0; i < 100; i++)
+                {
+                    int x = random.Next(image.Width);
+                    int y = random.Next(image.Height);
+                    image.SetPixel(x,y,Color.FromArgb(random.Next()));
+                }
+                g.DrawRectangle(new Pen(Color.Silver), 0, 0, image.Width - 1, image.Height - 1);
+                MemoryStream stream = new MemoryStream();
+                image.Save(stream, ImageFormat.Jpeg);
+                return stream.ToArray();
+            }
+            finally
+            {
+                g.Dispose();
+                image.Dispose();  
+            }
+            
+        }
+        public string getCode(int len)
+        {
+            string words = "abcdefghijkmnpqstuvwxyABCDEFGHJKMNPQSTUVWXY3456789";
+            StringBuilder code = new StringBuilder();
+            Random random = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                code.Append(words[random.Next(0, words.Length)]);
+            }
+            return code.ToString();
+
+        }
+    }
+}
---
+++ UI/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UI.Models
+{
+    public class LoginViewModel 
+    {
+        public string LoginName { get; set; }
+        public string LoginPwd { get; set; }
+        public string Code { get; set; }
+        public bool IsRemember { get; set; }
+    }
+}
