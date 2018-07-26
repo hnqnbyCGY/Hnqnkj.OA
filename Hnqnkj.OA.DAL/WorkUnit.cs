899033aa46c7fd31e21489894129b818c51b1f09
阿斯顿 <1250611200@qq.com>
2018-07-26 16:55:09 +0800

    456465

--- Hnqnkj.OA.DAL/WorkUnit.cs
+++ Hnqnkj.OA.DAL/WorkUnit.cs
@@ -42,5 +42,40 @@ namespace Hnqnkj.OA.DAL
             }
         }
 
+        private CommonRepository<Specialty> _specialty;
+        public CommonRepository<Specialty> Specialty
+        {
+            get
+            {
+                if (_specialty == null)
+                    _specialty = new CommonRepository<Model.Specialty>(DB);
+                return _specialty;
+            }
+        }
+
+        private CommonRepository<Team> _team;
+        public CommonRepository<Team> Team
+        {
+            get
+            {
+                if(_team == null)
+                {
+                    _team = new CommonRepository<Model.Team>(DB);
+                }
+                return _team;
+            }
+        }
+
+        private CommonRepository<Student> _student;
+        public CommonRepository<Student> Student
+        {
+            get
+            {
+                if (_student == null)
+                    _student = new CommonRepository<Model.Student>(DB);
+                return _student;
+            }
+        }
+        
     }
 }
