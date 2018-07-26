0733fc767b2f9c2f57fe206da1bea93b9e6de773
tyz <812130678@qq.com>
2018-07-26 14:46:17 +0800

    2

---
+++ Hnqnkj.OA.Common/DES.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Hnqnkj.OA.Common
+{
+    /// DES对称加密
+    /// </summary>
+    public sealed class DES
+    {
+        /// <summary>
+        /// Des解密方法
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="key">8位的key</param>
+        /// <param name="IV">8位的IV</param>
+        /// <returns></returns>
+        public static string Decrypt(string val, string key, string IV)
+        {
+            try
+            {
+                byte[] buffer1 = Encoding.UTF8.GetBytes(key);
+                byte[] buffer2 = Encoding.UTF8.GetBytes(IV);
+                DESCryptoServiceProvider provider1 = new DESCryptoServiceProvider();
+                provider1.Mode = CipherMode.ECB;
+                provider1.Key = buffer1;
+                provider1.IV = buffer2;
+                ICryptoTransform transform1 = provider1.CreateDecryptor(provider1.Key, provider1.IV);
+                byte[] buffer3 = Convert.FromBase64String(val);
+                MemoryStream stream1 = new MemoryStream();
+                CryptoStream stream2 = new CryptoStream(stream1, transform1, CryptoStreamMode.Write);
+                stream2.Write(buffer3, 0, buffer3.Length);
+                stream2.FlushFinalBlock();
+                stream2.Close();
+                return Encoding.UTF8.GetString(stream1.ToArray());
+            }
+            catch (System.Exception ex)
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Des加密方法
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="key"></param>
+        /// <param name="IV"></param>
+        /// <returns></returns>
+        public static string Encrypt(string val, string key, string IV)
+        {
+            try
+            {
+
+                byte[] buffer1 = Encoding.UTF8.GetBytes(key);
+                byte[] buffer2 = Encoding.UTF8.GetBytes(IV);
+
+                DESCryptoServiceProvider provider1 = new DESCryptoServiceProvider();
+                provider1.Mode = CipherMode.ECB;
+                provider1.Key = buffer1;
+                provider1.IV = buffer2;
+                ICryptoTransform transform1 = provider1.CreateEncryptor(provider1.Key, provider1.IV);
+                byte[] buffer3 = Encoding.UTF8.GetBytes(val);
+                MemoryStream stream1 = new MemoryStream();
+                CryptoStream stream2 = new CryptoStream(stream1, transform1, CryptoStreamMode.Write);
+                stream2.Write(buffer3, 0, buffer3.Length);
+                stream2.FlushFinalBlock();
+                stream2.Close();
+                return Convert.ToBase64String(stream1.ToArray());
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+
+    }
+}
--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -4,30 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Hnqnkj.OA.DAL;
+using System.Web.Security;
+using Newtonsoft.Json;
+using Hnqnkj.OA.Model;
+using Hnqnkj.OA.Common;
 
 namespace UI.Controllers
 {
     public class HomeController : Controller
     {
-        OADBContext context = new OADBContext();
-  
+
+        WorkUnit Work = new WorkUnit();
+
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Login(string name,string pwd)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var user = new { name = "admin", pwd = "123456" };
+            string userdata = JsonConvert.SerializeObject(user);
+            FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
+                user.name, DateTime.Now, DateTime.Now.AddDays(1), true, userdata, FormsAuthentication.CookieDomain);
+            HttpCookie co = new HttpCookie(FormsAuthentication.FormsCookieName, FormsAuthentication.Encrypt(ticket));
+            Response.Cookies.Add(co);
+                HttpCookie coLoginName = new HttpCookie("Key", DES.Encrypt(user.name, "12345678", "87654321"));
+                coLoginName.Expires = DateTime.Now.AddYears(1);
+                Response.Cookies.Add(coLoginName);
+                HttpCookie coLoginPwd = new HttpCookie("Value", user.pwd);
+                coLoginPwd.Expires = DateTime.Now.AddYears(1);
+            Response.Cookies.Add(coLoginPwd);
+            return Json(new { success = true });
         }
+        [HttpPost]
+        //public ActionResult AutoLogin()
+        //{
+
+        //}
     }
 }
