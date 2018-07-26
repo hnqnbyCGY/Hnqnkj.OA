5930fe38edf8d73455658b408028a2e9b1a097c2
lh <1278129242@qq.com>
2018-07-26 15:16:38 +0800

    1

--- Hnqnkj.OA.DAL/OADBContext.cs
+++ Hnqnkj.OA.DAL/OADBContext.cs
@@ -16,22 +16,46 @@ namespace Hnqnkj.OA.DAL
 
         }
         public DbSet<AdminUser> AdminUsers { get; set; }
-
+        /// <summary>
+        /// 学校，校区
+        /// </summary>
         public DbSet<Shcool> Shcools { get; set; }
-
+        /// <summary>
+        /// 学生，客户
+        /// </summary>
         public DbSet<Student> Students { get; set; }
-
+        /// <summary>
+        /// 专业
+        /// </summary>
         public DbSet<Specialty> Specialtys { get; set; }
-
+        /// <summary>
+        /// 沟通类型
+        /// </summary>
         public DbSet<ConsultingType> ConsultingTypes { get; set; }
-
+        /// <summary>
+        /// 咨询沟通方式
+        /// </summary>
         public DbSet<ConsultingWay> ConsultingWays { get; set; }
-
+        /// <summary>
+        /// 咨询专业
+        /// </summary>
         public DbSet<ConsultMajor> ConsultMajor { get; set; }
-
+        /// <summary>
+        /// 客户来源
+        /// </summary>
         public DbSet<CustomerSource> CustomerSource { get; set; }
-
+        /// <summary>
+        /// 客户状态
+        /// </summary>
         public DbSet<CustomerState> CustomerState { get; set; }
+        /// <summary>
+        /// 班级
+        /// </summary>
+        public DbSet<Team> Teams { get; set; }
+        /// <summary>
+        /// 意向程度
+        /// </summary>
+        public DbSet<IntentionDegree> IntentionDegrees { get; set; }
        
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
