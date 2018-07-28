3b7226586b894463c4dde600aef904c533ae881d
tyz <812130678@qq.com>
2018-07-28 16:22:24 +0800

    2

---
+++ UI/Controllers/ChatListController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hnqnkj.OA.Model;
+using Hnqnkj.OA.DAL;
+
+namespace UI.Controllers
+{
+    public class ChatListController : Controller
+    {
+        WorkUnit work = new WorkUnit();
+        // GET: ChatList
+        public ActionResult Index(int page = 0, int offset=0 ,int limit = 10, string sort = "Id",OrderMode order= OrderMode.Asc)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                var list = work.CommunicationRecord.GetPageEntitys(m=>1==1,limit,offset,sort,order).ToList();
+                return Json(new { code=0,count=list.Count,data=list},JsonRequestBehavior.AllowGet);
+            }
+            return View();
+        }
+    }
+}
