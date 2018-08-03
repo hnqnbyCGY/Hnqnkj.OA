4e5d9b4a9c0c97c739c17a368204bbe4585212a3
tyzma <812130678@qq.com>
2018-08-03 14:55:51 +0800

    11

---
+++ Hnqnkj.OA.Common/MD5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hnqnkj.OA.Common
+{
+    public class MD5
+    {
+        public static string GetMD5(string str)
+        {
+            byte[] b = System.Text.Encoding.Default.GetBytes(str);
+
+            b = new System.Security.Cryptography.MD5CryptoServiceProvider().ComputeHash(b);
+            string ret = "";
+            for (int i = 0; i < b.Length; i++)
+            {
+                ret += b[i].ToString("X").PadLeft(2, '0');
+            }
+            return ret;
+        }
+    }
+}
